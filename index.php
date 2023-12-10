0c748bb6e14b4a7c000fb69ceda60ce211219281
Whoislead <94552006+Whoislead@users.noreply.github.com>
2023-12-09 22:22:19 -0800

    addition of index, view users and sidebar
    index file is the html creator, view users allows for listing users

---
+++ index.php
@@ -0,0 +1,22 @@
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="style.css" type = "text/css"/><!--CSS file added -->
+    <title>Dolphin CRM</title>
+</head>
+<body class = "container">
+    <?php require "adminChecker.php"?>
+    <header>
+        <div>
+            <img src="" alt=""><!--Logo-->
+            <p>DOLPHIN CRM</p>
+        </div>
+    </header>
+    <div id = "gridBox">
+    </div>
+    <!--Footer containing date using php date function -->
+    <?php include "footer.php";?>
+</body>
+</html>
---
+++ sideBar.php
@@ -0,0 +1,9 @@
+<section id  = "sideBar">
+    <ul>
+        <li><a href=""><img src="houseIcon.png" alt="Symbol for home">Home</a></li>
+        <li><a href=""><img src="newContactIcon.png" alt="Symbol for new contact">New Contact</a></li>
+        <li><a href=""><img src="userIcon.png" alt="Symbol for user">Users</a></li>
+    <hr>
+        <li><a href=""><img src="logoutIcon.png" alt="Symbol for logput">Logout</a></li>
+    </ul>
+</section>
